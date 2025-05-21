67c7db1d9e081529939c1639addd192234ec190e
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-05-21 12:11:59 +0200

    Feat(project): Update MovieViewModel code

--- app/src/main/java/com/esgi/moviesapp/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/esgi/moviesapp/viewmodel/MovieViewModel.kt
@@ -1,10 +1,14 @@
 package com.esgi.moviesapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.esgi.moviesapp.data.model.TvShow
 import com.esgi.moviesapp.data.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MovieViewModel(
     private val movieRepository : MovieRepository
@@ -15,4 +19,11 @@ class MovieViewModel(
 
     val tvShows : StateFlow<List<TvShow>> = _tvShows
 
+    fun getTvShows(page : Int = 1) = viewModelScope.launch(
+        Dispatchers.IO
+    ) {
+        movieRepository.getTvShows(page).collectLatest { tvShows ->
+            _tvShows.value = tvShows
+        }
+    }
 }
