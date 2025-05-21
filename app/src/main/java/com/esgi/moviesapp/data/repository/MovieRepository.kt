0e9251cb861822fe71e6035bc6298d8b4edb94f4
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-05-21 11:52:28 +0200

    Feat(project): Update repository class file code

--- app/src/main/java/com/esgi/moviesapp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/esgi/moviesapp/data/repository/MovieRepository.kt
@@ -3,13 +3,21 @@ package com.esgi.moviesapp.data.repository
 import com.esgi.moviesapp.data.model.TvShow
 import com.esgi.moviesapp.data.service.MovieService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 
 class MovieRepository(
     private val movieService: MovieService
 ) {
 
-    suspend fun getTvShows(page: Int = 1): List<TvShow> {
-        return movieService.getMoviesResponse(page)
-            .tvShows
+//    suspend fun getTvShows(page: Int = 1): List<TvShow> {
+//        return movieService.getMoviesResponse(page)
+//            .tvShows
+//    }
+
+    fun getTvShows(page: Int = 1): Flow<List<TvShow>> = flow {
+        emit(movieService.getMoviesResponse(page).tvShows)
     }
 }
+
+
