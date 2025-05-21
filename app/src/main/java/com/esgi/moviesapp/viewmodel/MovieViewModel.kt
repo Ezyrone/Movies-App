d82deea7488882e693c21e767b79c80c2623f2a9
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-05-21 12:06:28 +0200

    Feat(project): Update MovieViewModel code

--- app/src/main/java/com/esgi/moviesapp/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/esgi/moviesapp/viewmodel/MovieViewModel.kt
@@ -1,11 +1,18 @@
 package com.esgi.moviesapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.esgi.moviesapp.data.model.TvShow
 import com.esgi.moviesapp.data.repository.MovieRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MovieViewModel(
     private val movieRepository : MovieRepository
 ): ViewModel() {
 
+    private var _tvShows : MutableStateFlow<List<TvShow>> =
+        MutableStateFlow(emptyList())
+
+    val tvShows : StateFlow<List<TvShow>> = _tvShows
 
 }
