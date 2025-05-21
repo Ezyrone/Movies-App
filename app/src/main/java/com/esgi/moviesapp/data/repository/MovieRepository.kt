6f0b482b7ac78b18899a4720c6b7eb535a2242f2
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-05-21 11:47:04 +0200

    Feat(project): Add repository class file code

--- app/src/main/java/com/esgi/moviesapp/data/repository/MovieRepository.kt
+++ app/src/main/java/com/esgi/moviesapp/data/repository/MovieRepository.kt
@@ -1,9 +1,15 @@
 package com.esgi.moviesapp.data.repository
 
+import com.esgi.moviesapp.data.model.TvShow
 import com.esgi.moviesapp.data.service.MovieService
+import kotlinx.coroutines.flow.Flow
 
 class MovieRepository(
     private val movieService: MovieService
 ) {
 
+    suspend fun getTvShows(page: Int = 1): List<TvShow> {
+        return movieService.getMoviesResponse(page)
+            .tvShows
+    }
 }
