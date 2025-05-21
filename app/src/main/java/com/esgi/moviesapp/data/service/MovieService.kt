16a6408afeec1a04c9554e0a9bbfd8ece5248a3f
JørR <43743804+Ezyrone@users.noreply.github.com>
2025-05-21 11:16:42 +0200

    Feat(project): Add code into interface

--- app/src/main/java/com/esgi/moviesapp/data/service/MovieService.kt
+++ app/src/main/java/com/esgi/moviesapp/data/service/MovieService.kt
@@ -1,5 +1,10 @@
 package com.esgi.moviesapp.data.service
 
+import com.esgi.moviesapp.data.model.MovieResponse
+import retrofit2.http.GET
+
 interface MovieService {
 
+    @GET("most-popular")
+    fun getMoviesResponse() : MovieResponse
 }
